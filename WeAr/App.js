2866101a9b9fc64fd2353850974504b5cedbe988
louisohlow <l.ohlow@sevensenders.com>
2020-10-10 22:43:37 +0200

    code cleanup

--- WeAr/App.js
+++ WeAr/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import SplashScreen from 'react-native-splash-screen';
-import { Text } from 'react-native';
 import PermissionContainer from './js/components/molecules/permission/PermissionContainer';
-import ArEntry from './js/ar/ArEntry';
+import ARContainer from './js/components/molecules/ar/ARContainer';
 
 class App extends React.Component {
   componentDidMount() {
@@ -12,7 +11,7 @@ class App extends React.Component {
   render() {
     return (
       <PermissionContainer>
-        <ArEntry />
+        <ARContainer />
       </PermissionContainer>
     );
   }
