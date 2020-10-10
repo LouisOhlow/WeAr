e7dc36ce11db60510563884c7a15c06b9957f85d
louisohlow <l.ohlow@sevensenders.com>
2020-10-10 21:15:16 +0200

    added react-viro setup changes

--- WeAr/android/app/src/main/java/com/wear/MainApplication.java
+++ WeAr/android/app/src/main/java/com/wear/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
+import com.viromedia.bridge.ReactViroPackage;
 
 
 import java.util.Arrays;
@@ -28,7 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
           new SplashScreenReactPackage(),
-          new RNPermissionsPackage()
+          new RNPermissionsPackage(),
+          new ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf(BuildConfig.VR_PLATFORM))
       );
     }
 
