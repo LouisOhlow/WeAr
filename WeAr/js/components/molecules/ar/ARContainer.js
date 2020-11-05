4f6bb68e46ac654bf32a81af4e9f80c99f2aa576
louisohlow <l.ohlow@sevensenders.com>
2020-11-05 22:03:39 +0100

    added photo animation

--- WeAr/js/components/molecules/ar/ARContainer.js
+++ WeAr/js/components/molecules/ar/ARContainer.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ToastAndroid } from 'react-native';
+import { View, StyleSheet, Animated } from 'react-native';
 import { ViroARSceneNavigator } from 'react-viro';
 import ScreenshotButton from '../../atoms/camera/ScreenshotButton';
 import ARCamera from './ARCamera';
-import curDateTime from '../../../utils/curDateTime';
-
-const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-  },
-});
+import curDateTime from '../../../utils/time/curDateTime';
 
 export default class ARContainer extends Component {
   constructor() {
@@ -17,10 +11,27 @@ export default class ARContainer extends Component {
     this._arScene = React.createRef();
   }
 
-  state = { isRecording: false };
+  state = { 
+    isRecording: false,
+    fadeAnimation: new Animated.Value(0)
+  };
+
+  photoAnimation = () => {
+    Animated.sequence([
+      Animated.timing(this.state.fadeAnimation, {
+        toValue: 1,
+        duration: 10
+      }),
+      Animated.timing(this.state.fadeAnimation, {
+        toValue: 0,
+        duration: 40
+      })
+    ]).start();
+  };
 
   capturePhoto = async () => {
-    const foto = await this._arScene.sceneNavigator.takeScreenshot(curDateTime(), true);
+    await this._arScene.sceneNavigator.takeScreenshot(curDateTime(), true);
+    this.photoAnimation();
   }
 
   startVideo = () => {
@@ -31,7 +42,7 @@ export default class ARContainer extends Component {
   
   stopVideo = async () => {
     if(this.state.isRecording){
-      const video = await this._arScene.sceneNavigator.stopVideoRecording();
+      await this._arScene.sceneNavigator.stopVideoRecording();
       this.setState({isRecording: false});
     }
   }
@@ -39,14 +50,28 @@ export default class ARContainer extends Component {
   render(){
     return (
       <View style={styles.container}>
-        <ViroARSceneNavigator
+       <ViroARSceneNavigator
           ref={(c) => this._arScene = c}
           initialScene={{ scene: ARCamera }}
           autofocus={true}
         />
+        <Animated.View style={[styles.camAnimation, {opacity: this.state.fadeAnimation}]}/> 
         <ScreenshotButton capturePhoto={() => this.capturePhoto() } startVideo={() => {this.startVideo()}} stopVideo={() => {this.stopVideo()}} />
       </View>
     );
   }
   
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+  },
+  camAnimation: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    alignSelf: 'center',
+    backgroundColor: 'black',
+  }
+});
---
+++ WeAr/js/utils/time/curDateTime.js
@@ -0,0 +1,4 @@
+export default function curDateTime() {
+  const date = new Date();
+  return `${date.getFullYear()}${date.getMonth()}${date.getDate()}-${date.getHours()}${date.getMinutes()}${date.getSeconds()}${date.getMilliseconds()}`;
+}
