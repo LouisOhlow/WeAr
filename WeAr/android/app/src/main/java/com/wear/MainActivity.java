a156181af5779c600aea7333b7a38e1469f8b55d
louisohlow <l.ohlow@sevensenders.com>
2020-11-09 18:21:09 +0100

    react-navigation setup

--- WeAr/android/app/src/main/java/com/wear/MainActivity.java
+++ WeAr/android/app/src/main/java/com/wear/MainActivity.java
@@ -2,6 +2,9 @@ package com.wear;
 
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -14,4 +17,14 @@ public class MainActivity extends ReactActivity {
         SplashScreen.show(this);  // here
         return "WeAr";
     }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
 }
--- WeAr/android/app/src/main/java/com/wear/MainApplication.java
+++ WeAr/android/app/src/main/java/com/wear/MainApplication.java
@@ -3,6 +3,10 @@ package com.wear;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+import com.swmansion.rnscreens.RNScreensPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.swmansion.reanimated.ReanimatedPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,6 +31,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SafeAreaContextPackage(),
+            new RNScreensPackage(),
+            new RNGestureHandlerPackage(),
+            new ReanimatedPackage(),
           new SplashScreenReactPackage(),
           new ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf(BuildConfig.VR_PLATFORM))
       );
