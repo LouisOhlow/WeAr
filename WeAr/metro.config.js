a69a8831cfdf9d8cd3070ed964e92d5c2b05f64d
louisohlow <l.ohlow@sevensenders.com>
2020-11-26 22:36:50 +0100

    added config for vrx and obj files

--- WeAr/metro.config.js
+++ WeAr/metro.config.js
@@ -1,17 +1,15 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+'use strict';
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-};
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { assetExts }
+  } = await getDefaultConfig();
+
+  return {
+    resolver: {
+      assetExts: [...assetExts, "obj", "mtl", "JPG", "vrx", "hdr", "gltf", "glb", "bin", "arobject", "gif"]
+    }
+  };
+})();
