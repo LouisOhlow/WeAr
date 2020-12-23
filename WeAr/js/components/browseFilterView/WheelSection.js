b192fb77b9c0d44adcbbc9fd3eecddad1afd6f28
louisohlow <l.ohlow@sevensenders.com>
2020-12-23 20:22:28 +0100

    smoll fiks

--- WeAr/js/components/browseFilterView/WheelSection.js
+++ WeAr/js/components/browseFilterView/WheelSection.js
@@ -2,56 +2,54 @@ import React from 'react';
 import {
   View, StyleSheet, FlatList,
 } from 'react-native';
-import WheelBubble from './WheelBubble';
 import { connect } from 'react-redux';
+import WheelBubble from './WheelBubble';
 import { setFilterIndex } from '../../actions/filter';
 import { getFiltersByNode } from '../../data/db/dataController';
 import { openRealm } from '../../data/db/realmController';
-
 
 class WheelSection extends React.Component {
   constructor() {
     super();
     this.state = ({
       scrollPos: 0,
-      filterList: []
+      filterList: [],
     });
   }
 
   componentDidMount() {
-    const { filter } = this.props
+    const { filter } = this.props;
     const realm = openRealm();
-    const filterResults = getFiltersByNode(realm, filter.selectedNode)
-    filterList = []
+    const filterResults = getFiltersByNode(realm, filter.selectedNode);
+    const filterList = [];
 
-    filterList.push({id: 'add'})
-    for (let filter of filterResults) {
-      filterList.push(filter)
+    filterList.push({ id: 'add' });
+    for (const filter of filterResults) {
+      filterList.push(filter);
     }
-    filterList.push({id: 'end'})
+    filterList.push({ id: 'end' });
 
     this.setState({
-      filterList
-    })
+      filterList,
+    });
   }
 
   handleScroll = (event) => {
-    const scrollPos = event.nativeEvent.contentOffset.x
+    const scrollPos = event.nativeEvent.contentOffset.x;
     this.setState({
       scrollPos,
     });
-    if(scrollPos === 0) {
-      this.props.setSelectedIndex(0)
-    }
-    else if((scrollPos%120) === 0) {
-      this.props.setSelectedIndex(scrollPos/120)
+    if (scrollPos === 0) {
+      this.props.setSelectedIndex(0);
+    } else if ((scrollPos % 120) === 0) {
+      this.props.setSelectedIndex(scrollPos / 120);
     }
   }
 
   render() {
     const { scrollPos, filterList } = this.state;
 
-    const tempList = [...filterList]
+    const tempList = [...filterList];
 
     return (
       <View styles={styles.container}>
@@ -85,7 +83,7 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const mapStateToProps = (state) => ({
-  filter: state.filterRed.filter
+  filter: state.filterRed.filter,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(WheelSection);
