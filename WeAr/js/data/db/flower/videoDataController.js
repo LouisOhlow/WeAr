b6a44249c65fd8678e43206b2b6a95b1cfbff69a
louisohlow <l.ohlow@sevensenders.com>
2021-01-02 00:28:23 +0100

    added video data retriever

---
+++ WeAr/js/data/db/flower/videoDataController.js
@@ -0,0 +1,39 @@
+import SCREENS from '../../../navigation/navigationScreens';
+import {
+  FILTER_SCHEMA, MEDIA_SCHEMA,
+} from '../Schemas';
+
+/**
+ * fetches the primary and secondary color from the chosen filter
+ *
+ * @param {object} realm an opened realm connection
+ * @param {number} index the chosen flower index
+ * @returns {object} the video uri
+ */
+export function getFlowervideoByIndex(realm, index) {
+  const filter = realm.objects(FILTER_SCHEMA).filtered(`node = '${SCREENS.flower}' AND index = '${index}'`)[0];
+
+  const videoId = filter.media[0];
+  const mediaObject = realm.objects(MEDIA_SCHEMA).filtered(`id = '${videoId}'`)[0];
+
+  const uri = mediaObject.media;
+
+  return uri;
+}
+
+/**
+ * updates the video source from the chosen filter in the realm db
+ *
+ * @param {object} realm an opened realm connection
+ * @param {number} index the chosen flower index
+ * @param {colors} uri uri from the video
+ */
+export function setFlowervideoByIndex(realm, index, uri) {
+  const filter = realm.objects(FILTER_SCHEMA).filtered(`node = '${SCREENS.flower}' AND index = '${index}'`)[0];
+
+  const videoId = filter.media[0];
+
+  realm.write(() => {
+    realm.create(MEDIA_SCHEMA, { id: videoId, media: uri }, 'modified');
+  });
+}
