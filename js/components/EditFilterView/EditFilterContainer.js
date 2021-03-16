ee102f86c48b53a0a3b7be8ea4d5c39c602be068
louisohlow <l.ohlow@sevensenders.com>
2021-03-16 14:27:11 +0100

    added create/save button in footer

--- js/components/EditFilterView/EditFilterContainer.js
+++ js/components/EditFilterView/EditFilterContainer.js
@@ -7,27 +7,20 @@ import { setFilterIndex } from '../../actions/filter';
 import COLORS from '../../res/colors';
 import SettingsFooter from './SettingsFooter';
 import SettingsHeader from './SettingsHeader';
-import SettingsContainer from './SettingsContainer';
 
 /**
  * contains the settings for the flower filter
  */
-class EditFilterContainer extends React.Component {
-  render() {
-    const newFilter = true;
+function EditFilterContainer(props) {
+  const newFilter = true;
 
-    return (
-      <View>
-        <SettingsHeader />
-        <View style={styles.body} />
-        <View style={styles.footer}>
-          <View style={styles.setting} />
-          <View style={styles.menu} />
-          <View style={styles.buttonsection} />
-        </View>
-      </View>
-    );
-  }
+  return (
+    <View>
+      <SettingsHeader />
+      <View style={styles.body} />
+      <SettingsFooter newFilter={newFilter} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -36,27 +29,6 @@ const styles = StyleSheet.create({
     width: '100%',
     backgroundColor: 'rgba(0, 100, 0, 0.5)',
     justifyContent: 'space-around',
-  },
-  footer: {
-    height: '35%',
-    width: '100%',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 100, 0.5)',
-  },
-  setting: {
-    width: '100%',
-    height: '35%',
-    backgroundColor: 'rgba(255, 50, 50, 0.5)',
-  },
-  menu: {
-    width: '100%',
-    height: '20%',
-    backgroundColor: 'rgba(150, 150, 150, 0.5)',
-  },
-  buttonsection: {
-    width: '100%',
-    height: '30%',
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
   },
 });
 
--- js/components/EditFilterView/SettingsFooter.js
+++ js/components/EditFilterView/SettingsFooter.js
@@ -4,12 +4,18 @@ import COLORS from '../../res/colors';
 import AppButton from '../basics/AppButton';
 
 function SettingsFooter(props) {
-  const { styling, title } = props;
+  const { newFilter } = props;
 
   return (
-    <View style={styles.container}>
-      <View style={styles.button}>
-        <AppButton title={title} styling={styling} />
+    <View style={styles.footer}>
+      <View style={styles.setting} />
+      <View style={styles.menu} />
+      <View style={styles.buttonsection}>
+        <View style={styles.delete} />
+        <View style={styles.save}>
+          <AppButton title={newFilter ? 'CREATE' : 'SAVE'} styling="apply" />
+        </View>
+        <View style={styles.revert} />
       </View>
     </View>
   );
@@ -25,6 +31,50 @@ const styles = StyleSheet.create({
   button: {
     alignSelf: 'center',
     width: 150,
+  },
+  footer: {
+    height: '35%',
+    width: '100%',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 100, 0.5)',
+  },
+  setting: {
+    width: '100%',
+    height: '35%',
+    backgroundColor: 'rgba(255, 50, 50, 0.5)',
+  },
+  menu: {
+    width: '100%',
+    height: '20%',
+    backgroundColor: 'rgba(150, 150, 150, 0.5)',
+  },
+  buttonsection: {
+    width: '100%',
+    height: '30%',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    flexDirection: 'row',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  delete: {
+    width: '20%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 255, 255, 0.5)',
+    flexDirection: 'row',
+  },
+  save: {
+    width: '50%',
+    height: '100%',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  revert: {
+    width: '20%',
+    height: '100%',
+    backgroundColor: 'rgba(255, 255, 0, 0.5)',
+    flexDirection: 'row',
   },
 });
 
