ba3acca838f9c1ba4f9c16517ad561a5a4609327
louisohlow <l.ohlow@sevensenders.com>
2021-03-24 13:19:59 +0100

    added color box style

--- js/components/EditFilterView/settings/ColorSetting.js
+++ js/components/EditFilterView/settings/ColorSetting.js
@@ -24,6 +24,39 @@ class ColorSetting extends React.Component {
     };
   }
 
+  /**
+   * returns the color box style
+   *
+   * @param {string} colorType either 'secondaryColor' or 'primaryColor'
+   * dependending on the color box to style
+   */
+  getboxStyle() {
+    const { filter, setting } = this.props;
+    var color = 'blue';
+    if (filter.selectedMaterial.length > 0) {
+      const material = JSON.parse(JSON.stringify(filter.selectedMaterial));
+
+      const indeces = setting.forObject[0].split('-');
+      if (indeces.length === 1) {
+        color = material[0][indeces[0]].diffuseColor;
+      } else {
+        color = material[indeces[0]][indeces[1]].diffuseColor;
+      }
+    }
+
+    const box = {
+      width: 70,
+      height: 70,
+      margin: 15,
+      borderWidth: 3,
+      borderColor: COLORS.neutral,
+      borderRadius: 50,
+      backgroundColor: color,
+    };
+
+    return box;
+  }
+
   closePicker = (save) => {
     this.setState({ showPicker: false });
     if (save) {
@@ -44,11 +77,13 @@ class ColorSetting extends React.Component {
       if (indeces.length === 1) {
         const updatedMaterial = material.map((matList) => {
           matList[indeces[0]].id = color;
+          matList[indeces[0]].diffuseColor = color;
           return matList;
         });
         this.props.setMaterial(updatedMaterial);
       } else {
         material[indeces[0]][indeces[1]].id = color;
+        material[indeces[0]][indeces[1]].diffuseColor = color;
         this.props.setMaterial(material);
       }
     }
@@ -72,7 +107,7 @@ class ColorSetting extends React.Component {
           )
           : (
             <TouchableOpacity
-              style={styles.colorbox}
+              style={this.getboxStyle()}
               onPress={() => { this.setState({ showPicker: true }); }}
             />
           )}
