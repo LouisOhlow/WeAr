4bb1e6f73d3afcc2fcf8ebc34eaaa3c187b7eb03
louisohlow <l.ohlow@sevensenders.com>
2021-03-24 14:25:10 +0100

    added size update setting

--- js/components/EditFilterView/settings/SliderSetting.js
+++ js/components/EditFilterView/settings/SliderSetting.js
@@ -1,24 +1,54 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Slider } from 'react-native-gesture-handler';
+import { connect } from 'react-redux';
+import { setAugments } from '../../../actions/filter';
 
-function SliderSetting() {
-  return (
-    <View style={styles.container}>
-      <Slider
-        style={styles.slider}
-        value={0}
-        minimumValue={-0.5}
-        maximumValue={0.5}
-        minimumTrackTintColor="#FFFFFF"
-        maximumTrackTintColor="#000000"
-        onValueChange={() => {}}
-      />
-    </View>
-  );
+class SliderSetting extends React.Component {
+  /**
+   * sets the height and width of the video
+   *
+   * @param {number} ratio the slider value between -.5 and .5
+   */
+  updateSize(size) {
+    const { filter, setting } = this.props;
+    const augments = JSON.parse(JSON.stringify(filter.selectedAugments));
+    const findAugment = (augmentObject) => (augmentObject.id.toString() === setting.forObject[0]);
+    const index = augments.findIndex(findAugment);
+    augments[index].scale[0] = size;
+    augments[index].scale[1] = size;
+    augments[index].scale[2] = size;
+    //alert('update');
+    this.props.setAugments(augments);
+  }
+
+  render() {
+    //const { filter, setting } = this.props;
+    return (
+      <View style={styles.container}>
+        <Slider
+          style={styles.slider}
+          value={0.005}
+          minimumValue={0.005}
+          maximumValue={0.05}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#000000"
+          onValueChange={(value) => { this.updateSize(value); }}
+        />
+      </View>
+    );
+  }
 }
 
-export default SliderSetting;
+const mapStateToProps = (state) => ({
+  filter: state.filterRed.filter,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setAugments: (media) => dispatch(setAugments(media)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SliderSetting);
 
 const styles = StyleSheet.create({
   container: {
